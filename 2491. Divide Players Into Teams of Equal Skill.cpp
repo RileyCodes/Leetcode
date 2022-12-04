38576038bfe0b531a21765c2a370a9ea8fa45f0e
RileyCodes <35380563+RileyCodes@users.noreply.github.com>
2022-12-04 14:43:37 -0800

    Create 2491. Divide Players Into Teams of Equal Skill.cpp

---
+++ 2491. Divide Players Into Teams of Equal Skill.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+
+    /*
+        You are given a positive integer array skill of even length n where skill[i] denotes the skill of the ith player. Divide the players into n / 2 teams of size 2 such that the total skill of each team is equal.
+
+        The chemistry of a team is equal to the product of the skills of the players on that team.
+
+        Return the sum of the chemistry of all the teams, or return -1 if there is no way to divide the players into teams such that the total skill of each team is equal.    
+    */
+
+    //Divide the players into n / 2 teams: Since the skill array are even, it is always possibile to divide into n / 2 teams
+    //However, the total skill of each team must be equal.
+
+    //How to we know we can divide the players into teams such that that the total skill of each team is equal?
+
+
+
+    int teamSkills = -1;
+
+    sort(skill.begin(),skill.end());
+
+    int lP = 0, rP = skill.size() - 1;
+    vector<pair<int,int>> teams;
+    while(lP < rP)
+    {
+        if(teamSkills > 0)
+        {
+            if(skill[lP] + skill[rP] != teamSkills)
+                return -1;
+        }
+        else
+            teamSkills = skill[lP] + skill[rP];
+
+        teams.emplace_back(pair<int,int>(skill[lP],skill[rP]));
+        ++lP;
+        --rP;
+    }
+
+/*
+The chemistry of a team is equal to the product of the skills of the players on that team.
+
+Return the sum of the chemistry of all the teams, or return -1 if there is no way to divide the players into teams such that the total skill of each team is equal
+*/
+    long long sum = 0;
+    for(auto team : teams)
+    {
+        auto che = team.first * team.second;
+        sum += che;
+    }
+
+    //dummy return for now.
+    return sum;
+    }
+};
