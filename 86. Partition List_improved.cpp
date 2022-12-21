0edc899991aa5311fca7dca1c91e4584f43372ad
RileyCodes <35380563+RileyCodes@users.noreply.github.com>
2022-12-20 16:11:08 -0800

    Update 86. Partition List_improved.cpp

--- 86. Partition List_improved.cpp
+++ 86. Partition List_improved.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
-
-    ListNode* partition(ListNode* head, auto& lP, auto& geP, int x, ListNode*& gePhead, ListNode*& lPhead)
+ListNode* partition(ListNode* head, ListNode*& lP, ListNode*& geP, int x, ListNode*& gePhead, ListNode*& lPhead)
+{
+    if(head == nullptr)
     {
-        if(head == nullptr)
-        {
-            if(lP)
-                lP->next = nullptr;
-            if(geP)
-                geP->next = nullptr;
-            return lP;
-        }
-        auto* node = nullptr;
-        if(head->val < x)
-        {
-            node = &lP;
-            if(lPhead == nullptr)
-                lPhead = head;            
-        }
-        else
-        {
-            node = &geP;
-            if(gePhead == nullptr)
-                gePhead = head;
-        }
-            
-        if(!(*node))
-        {
-            (*node) = head;
-            return partition(head->next, lP,geP, x, gePhead,lPhead);
-        }
-        else
-        {
-            (*node)->next = head;
-            (*node) = head;
-            return partition(head->next, lP,geP, x, gePhead,lPhead);
-        }
-
-        return nullptr;
+        if(lP)
+            lP->next = nullptr;
+        if(geP)
+            geP->next = nullptr;
+        return lP;
     }
-
-    ListNode* partition(ListNode* head, int x) 
+    ListNode*& node = head->val < x ? lP : geP;
+    if(node == nullptr)
     {
-        ListNode *lP = nullptr, *geP = nullptr, *gepHead = nullptr, *lPhead = nullptr;
-        auto* lastlP = partition(head, lP,geP,x, gepHead, lPhead);
-        if(lastlP)
-            lastlP->next = gepHead;
-        return lPhead ? lPhead : (gepHead? gepHead: head);
+        node = head;
+        if(node == lP)
+            lPhead = head;
+        else
+            gePhead = head;
     }
+    else
+    {
+        node->next = head;
+        node = head;
+    }
+    return partition(head->next, lP, geP, x, gePhead, lPhead);
+}
+
+ListNode* partition(ListNode* head, int x) 
+{
+    ListNode *lP = nullptr, *geP = nullptr, *gepHead = nullptr, *lPhead = nullptr;
+    auto* lastlP = partition(head, lP,geP,x, gepHead, lPhead);
+    if(lastlP)
+        lastlP->next = gepHead;
+    return lPhead ? lPhead : (gepHead? gepHead: head);
+}
+
 };
